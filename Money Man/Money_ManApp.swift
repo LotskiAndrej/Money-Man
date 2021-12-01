89d41a33b142fd85e070d42b27afa566803f68c8
Andrej Lotski <andrej.lotski@webfactory.mk>
2021-12-01 18:36:07 +0100

    Initial Commit

---
+++ Money Man/Money_ManApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_ManApp.swift
+//  Money Man
+//
+//  Created by Andrej Lotski on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Money_ManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
